39fae8f1cdbde7b0a27047dc348b36cd0d14871a
Jeffrey Liu <jeffreyliu0117@gmail.com>
2021-11-24 00:29:52 -0500

    Add icons and stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,40 @@
-import { Flex, Text, Image, Box } from "@chakra-ui/react"
-import portrait from "../images/dark_portrait.png"
+import { Flex, Text, Image, Box, HStack, Icon, Link } from "@chakra-ui/react";
 
-// const portrait = require('../images/dark_portrait.png')
+/* Icons */
+import { HiOutlineMail } from "react-icons/hi";
+import { CgFileDocument } from "react-icons/cg";
+import { FiGithub } from "react-icons/fi";
+import { RiLinkedinLine } from "react-icons/ri";
+
+/* Assets */
+import portrait from "../static/dark_portrait.png";
+import Pdf from "../static/21-10-28.pdf";
 
 const Home = () => {
-    return (
-        <Flex bg="black" w="100vw" h="100vh" justify="center" align="center">
-            <Box>
-                <Image src={portrait} />
-            </Box>
-            <Flex direction="column">
-                <Text fontSize="6xl">
-                    Hi, I'm Jeffrey
-                </Text>
-            </Flex>
-        </Flex>
-    )
-}
+  return (
+    <Flex bg="black" w="100vw" h="100vh" justify="center" align="center">
+      <Box>
+        <Image src={portrait} />
+      </Box>
+      <Flex direction="column">
+        <Text fontSize="6xl">Hi, I'm Jeffrey</Text>
+        <HStack>
+          <Link href="mailto:jeffrey.liu@uwaterloo.ca">
+            <Icon as={HiOutlineMail} w={6} h={6} />
+          </Link>
+          <Link href="https://github.com/liu-jeffrey/" target="_blank">
+            <Icon as={FiGithub} w={6} h={6} />
+          </Link>
+          <Link href={Pdf} target="_blank">
+            <Icon as={CgFileDocument} w={6} h={6} />
+          </Link>
+          <Link href="https://www.linkedin.com/in/liu-jeffrey/" target="_blank">
+            <Icon as={RiLinkedinLine} w={6} h={6} />
+          </Link>
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default Home
+export default Home;
